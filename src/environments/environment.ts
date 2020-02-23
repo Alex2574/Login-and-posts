21e92dc1f530a6c7ed30137efda1d3874d085fa4
Alex2574 <aleksandr.strela@yahoo.com>
2020-02-23 10:23:44 +0200

    Fixed home page,when not created any post

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBeVNY1dO6dHpkj5e7EOZU2ctA4O-chAvQ',
-  fbDbUrl: 'https://angular-blog-e513b.firebaseio.com'
+  apiKey: 'AIzaSyAgSuxq0cjJCy6Ed4PNFFX1dTE8dTTjlFs',
+  fbDbUrl: 'https://angular-blog-15828.firebaseio.com'
 };
 
 /*
