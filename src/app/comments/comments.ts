ef2176b8da3a48fd8bea79873148181409993358
Alex2574 <aleksandr.strela@yahoo.com>
2020-03-29 16:43:27 +0300

    now can comment only users, which are logines

--- src/app/comments/comments.ts
+++ src/app/comments/comments.ts
@@ -1,28 +1,28 @@
-import { OnInit, Component, EventEmitter, Output } from "@angular/core";
+import { OnInit, Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommentBlock } from '../shared/interfaces';
 
 @Component({
-  selector: "app-comments",
-  templateUrl: "./comments.html",
-  styleUrls: ["./comments.scss"]
+  selector: 'app-comments',
+  templateUrl: './comments.html',
+  styleUrls: ['./comments.scss']
 })
 export class ContactComponent implements OnInit {
   name: string;
   email: string;
   message: string;
 
-
   @Output() messageToEmit = new EventEmitter<any>();
+  @Input() auth = '';
 
   constructor() {}
 
   ngOnInit() {}
 
   processForm() {
-
+    console.log(this.auth);
     const data = {
       author: this.name,
-      email: this.email,
+      email: localStorage.getItem('email'),
       message: this.message
     };
     this.messageToEmit.emit(data);
